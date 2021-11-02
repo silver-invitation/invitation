a33b55a21a2f933af82b5c5c0e95e572f4bde714
Keane <keane3pereira@gmail.com>
2021-11-02 22:41:45 +0530

    id test

--- res/guests/list.js
+++ res/guests/list.js
@@ -6,7 +6,9 @@ function getGuest(i) {
     
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
-    console.log(id); // "sai"
 
-    document.getElementById("to").innerHTML = "To, <br>" + guests[id]
+    var x = guests[id]
+    if (x) {
+        document.getElementById("to").innerHTML = "To, <br>" + x
+    }
 }
