b5f532dd67a6c914d13ba466d444168fe53501e0
Keane <keane3pereira@gmail.com>
2021-11-02 22:49:41 +0530

    whatsapp

--- res/guests/list.js
+++ res/guests/list.js
@@ -2,11 +2,11 @@ var guests = {
     1: "Carlyle, Lorraine, Mignonne & Shania"
 }
 
+
+
 function getGuest(i) {
-    
     const params = new URLSearchParams(window.location.search);
     const id = params.get("id");
-
     var x = guests[id]
     if (x) {
         document.getElementById("to").innerHTML = "To, <br>" + x
